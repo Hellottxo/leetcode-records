62ab967ad3afcd135a6313f072c4479b9ace3864
Hellottxo <654720416@qq.com>
2019-10-22 20:54:55 +0800

    add # Q217

--- JS-solutions/easy/Q217.js
+++ JS-solutions/easy/Q217.js
@@ -5,9 +5,9 @@
 var containsDuplicate = function(nums) {
   const map = {};
   nums.forEach(e => {
-      if(!map[e]) {
-          map[e] = e;
-      }
+    if(!map[e]) {
+      map[e] = e;
+    }
   })
   return Object.keys(map).length != nums.length;
 };
