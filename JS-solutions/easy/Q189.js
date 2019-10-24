c8bc1588418bd493a733d81a404c042179d286ed
Hellottxo <654720416@qq.com>
2019-10-24 21:22:28 +0800

    optimize the Q189

--- JS-solutions/easy/Q189.js
+++ JS-solutions/easy/Q189.js
@@ -4,8 +4,16 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-  for (let i = 0; i < k; i++) {
-    const item = nums.pop();
-    nums.unshift(item);
-  }
+  const len = nums.length - 1;
+  reverse(nums, 0, len);
+  reverse(nums, 0, k-1);
+  reverse(nums, len-k, len);
 };
+
+var reverse = function(nums, start, end) {
+  while(start < end) {
+    const tmp = nums[start];
+    nums[start++] = nums[end];
+    nums[end--] = tmp;
+  }
+}
